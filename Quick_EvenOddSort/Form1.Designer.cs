134241df4a628b7a0ab29ef6e7b50f8a941f9145
DelStez <kandakova0002@gmail.com>
2023-06-02 15:22:17 +0500

    Апдейт на больную голову

---
+++ Quick_EvenOddSort/Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace Quick_EvenOddSort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.zedGraphControl2 = new ZedGraph.ZedGraphControl();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.zedGraphControl3 = new ZedGraph.ZedGraphControl();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            this.tabPage3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(79, 434);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.button2.Location = new System.Drawing.Point(16, 586);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(138, 31);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "START";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.start_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Location = new System.Drawing.Point(63, 9);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(674, 419);
+            this.tabControl1.TabIndex = 23;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.zedGraphControl1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(578, 393);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Время Размер";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.IsPrintScaleAll = false;
+            this.zedGraphControl1.Location = new System.Drawing.Point(3, 6);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(569, 397);
+            this.zedGraphControl1.TabIndex = 0;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.zedGraphControl2);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(666, 393);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Перестановки время";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // zedGraphControl2
+            // 
+            this.zedGraphControl2.Location = new System.Drawing.Point(6, 9);
+            this.zedGraphControl2.Name = "zedGraphControl2";
+            this.zedGraphControl2.ScrollGrace = 0D;
+            this.zedGraphControl2.ScrollMaxX = 0D;
+            this.zedGraphControl2.ScrollMaxY = 0D;
+            this.zedGraphControl2.ScrollMaxY2 = 0D;
+            this.zedGraphControl2.ScrollMinX = 0D;
+            this.zedGraphControl2.ScrollMinY = 0D;
+            this.zedGraphControl2.ScrollMinY2 = 0D;
+            this.zedGraphControl2.Size = new System.Drawing.Size(646, 388);
+            this.zedGraphControl2.TabIndex = 24;
+            this.zedGraphControl2.UseExtendedPrintDialog = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.zedGraphControl3);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(765, 406);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Сравнения Время";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // zedGraphControl3
+            // 
+            this.zedGraphControl3.Location = new System.Drawing.Point(6, 9);
+            this.zedGraphControl3.Name = "zedGraphControl3";
+            this.zedGraphControl3.ScrollGrace = 0D;
+            this.zedGraphControl3.ScrollMaxX = 0D;
+            this.zedGraphControl3.ScrollMaxY = 0D;
+            this.zedGraphControl3.ScrollMaxY2 = 0D;
+            this.zedGraphControl3.ScrollMinX = 0D;
+            this.zedGraphControl3.ScrollMinY = 0D;
+            this.zedGraphControl3.ScrollMinY2 = 0D;
+            this.zedGraphControl3.Size = new System.Drawing.Size(753, 388);
+            this.zedGraphControl3.TabIndex = 25;
+            this.zedGraphControl3.UseExtendedPrintDialog = true;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label2.Location = new System.Drawing.Point(15, 434);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Входное";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(16, 460);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(138, 120);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(160, 460);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(106, 157);
+            this.textBox3.TabIndex = 24;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(272, 460);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(106, 157);
+            this.textBox4.TabIndex = 25;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label1.Location = new System.Drawing.Point(160, 434);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 23);
+            this.label1.TabIndex = 26;
+            this.label1.Text = "Быстрая";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label3.Location = new System.Drawing.Point(272, 434);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 23);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "Чет-Нечет";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(384, 460);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(395, 157);
+            this.textBox1.TabIndex = 28;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte) (204)));
+            this.label4.Location = new System.Drawing.Point(384, 434);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 23);
+            this.label4.TabIndex = 29;
+            this.label4.Text = "Логи";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.ClientSize = new System.Drawing.Size(791, 639);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "ComnEOSort";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage3.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+
+        private System.Windows.Forms.Label label4;
+
+        private ZedGraph.ZedGraphControl zedGraphControl3;
+
+        private ZedGraph.ZedGraphControl zedGraphControl2;
+
+        private System.Windows.Forms.TextBox textBox1;
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+
+        private System.Windows.Forms.TabPage tabPage3;
+
+        private System.Windows.Forms.TabControl tabControl1;
+
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+
+        private System.Windows.Forms.Label label2;
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+
+
+        #endregion
+    }
+}
---
+++ Quick_EvenOddSort/Form1.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZedGraph;
+
+namespace Quick_EvenOddSort
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+    int swapping = 0; int comprassion = 0; int swapping1 = 0; int comprassion2 = 0;
+    public List<double> numb = new List<double>();
+    public List<string> FilesPath = new List<string>();
+    public PointPairList list = new PointPairList ();
+    public PointPairList list2 = new PointPairList ();
+    public PointPairList list3 = new PointPairList ();
+    public PointPairList list4 = new PointPairList ();
+    public PointPairList list5 = new PointPairList ();
+    public PointPairList list6  = new PointPairList ();
+    public List<double> sizes = new List<double>();
+    public List<double> times1 = new List<double>();
+    public List<double> times2 = new List<double>();
+    public List<double> swapping_List = new List<double>();
+    public List<double> swapping2_List = new List<double>();
+    public List<double> comprassion_List = new List<double>();
+    public List<double> comprassion2_List = new List<double>();
+
+    public void DrawTimeSize(double size1, double time1, double time2, GraphPane pane, PointPairList x, PointPairList y, string text, string text1)
+    {
+        Random rnd = new Random ();
+        pane.CurveList.Clear ();
+        double ymax = ( (time1 * 1000) + 10 + (time2 * 1000) + 10) / 2;
+        // Заполняем список точек
+        x.Add (size1, time1 * 1000);
+        y.Add (size1, time2 * 1000);
+        // Создадим кривую
+        pane.AddCurve (text, x, Color.DeepPink, SymbolType.Circle);
+        pane.AddCurve (text1, y, Color.DarkBlue, SymbolType.Diamond);
+        pane.Legend.IsVisible = true;
+
+    }
+
+
+    private void Load_Click(object sender, EventArgs e)
+    {
+      Cleaning();
+      textBox2.Clear();
+      button2.Enabled = true;
+      OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+      openFileDialog1.Filter = "Текстовый файл (*.txt)|*.txt|Все файлы (*.*)|*.*";
+      openFileDialog1.FilterIndex = 1;
+      openFileDialog1.RestoreDirectory = true;
+
+      OpenFileDialog openFileDialog = new OpenFileDialog();
+      openFileDialog.Multiselect = true;
+      FilesPath.Clear();
+            
+
+      if (openFileDialog.ShowDialog() == DialogResult.OK)
+      {
+        foreach (string fileName in openFileDialog.FileNames)
+        {
+          FilesPath.Add(fileName);
+          textBox2.Text += fileName + ":\r\n";
+          string content = File.ReadAllText(fileName);
+          string[] texfile = content.Split(new string[] { "\n", " ", "\0" }, StringSplitOptions.RemoveEmptyEntries);
+          foreach (string n in texfile)
+          {
+            textBox2.Text += n.ToString() + "\r\n";
+          } 
+                    
+        }
+      }
+    }
+
+   private List<double> preStart(string FileName)
+        {
+            //
+            string content = File.ReadAllText(FileName);
+            string[] texfile = content.Split(new string[] { "\n", " ", "\0" }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (string Line in texfile)
+            {
+                double[] temp = Line.Split(new char[] { '\n', ' ', '\r' }).Select(double.Parse).ToArray();
+                for (int i = 0; i < temp.Length; i++)
+                {
+                    numb.Add(temp[i]);
+                }
+            }
+
+            return numb;
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            Cleaning();
+            foreach (var path in FilesPath)
+            {
+                List<double> numb = preStart(path);
+                sizes.Add(numb.Count);
+                textBox1.Text += path + ":\r\n";
+                var Start = DateTime.Now;
+                List<double> result = QuickSort(numb, 0, numb.Count - 1);
+                var spendtime = DateTime.Now - Start;
+                times1.Add(spendtime.TotalMilliseconds);
+                textBox3.Text += path + ":\r\n";
+                foreach (int n in result)
+                {
+                    textBox3.Text += n.ToString() + "\r\n";
+                }
+                swapping_List.Add(swapping);
+                comprassion_List.Add(comprassion);
+                numb.Clear();
+                textBox1.Text += "Расческа:" + "\r\n";
+                textBox1.Text += "Время:   " + spendtime.Milliseconds + " мс" + "\r\n";
+                textBox1.Text += "Кол-во перестановок: " + swapping.ToString() + ":\r\n";
+                textBox1.Text += "Кол-во сравнений: " + comprassion.ToString() + ":\r\n";
+                
+                
+                
+                numb= preStart(path);
+                var spendTimeForOne = spendtime;
+                Start = DateTime.Now;
+                result = EvenOddSort(numb);
+                spendtime = DateTime.Now - Start;
+                textBox4.Text += path +":\r\n";
+                foreach (int n in result)
+                {
+                    textBox4.Text += n.ToString() + "\r\n";
+                }
+                times2.Add(spendtime.TotalMilliseconds);
+                swapping2_List.Add(swapping1);
+                comprassion2_List.Add(comprassion2);
+                numb.Clear();
+                textBox1.Text += "Чет-нечет:" + "\r\n";
+                textBox1.Text += "Время:   " + spendtime.Milliseconds + " мс" + "\r\n";
+                textBox1.Text += "Кол-во перестановок: " + swapping1.ToString() + "\r\n";
+                textBox1.Text += "Кол-во сравнений: " + comprassion2.ToString() + "\r\n";
+  
+            }
+            MasterPane masterPane = zedGraphControl1.MasterPane;
+            MasterPane masterPane1 = zedGraphControl2.MasterPane;
+            MasterPane masterPane2 = zedGraphControl3.MasterPane;
+            
+            masterPane.PaneList.Clear ();
+            masterPane1.PaneList.Clear ();
+            masterPane2.PaneList.Clear ();
+            
+            GraphPane pane = new GraphPane ();
+            GraphPane pane1 = new GraphPane ();
+            GraphPane pane2 = new GraphPane ();
+            
+            for (int i = 0; i < sizes.Count; i++)
+            {
+                DrawTimeSize(sizes[i], times1[i], times2[i], pane, list, list2, "Быстрая", "Чет-Нечет");
+                DrawTimeSize(sizes[i], swapping_List[i], swapping2_List[i], pane1,  list3, list4, "Быстрая", "Чет-Нечет");
+                DrawTimeSize(sizes[i], comprassion_List[i], comprassion2_List[i], pane2,  list5, list6, "Быстрая", "Чет-Нечет");
+            }
+            masterPane.Add (pane);
+            masterPane1.Add (pane1);
+            masterPane2.Add (pane2);
+            
+            zedGraphControl1.AxisChange ();
+            zedGraphControl2.AxisChange ();
+            zedGraphControl3.AxisChange ();
+            
+            zedGraphControl1.Invalidate ();
+            zedGraphControl2.Invalidate ();
+            zedGraphControl3.Invalidate ();
+        }
+        
+        private void Cleaning()
+        {
+            textBox1.Clear();
+            textBox3.Clear();
+            textBox4.Clear();
+            textBox1.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            swapping = 0;
+            comprassion = 0; 
+            swapping1 = 0;
+            comprassion2 = 0;
+            list.Clear(); list3.Clear(); list4.Clear(); list4.Clear(); list5.Clear(); list6.Clear();
+            numb.Clear(); sizes.Clear(); times1.Clear(); times2.Clear(); swapping_List.Clear(); swapping2_List
+                .Clear(); comprassion_List.Clear(); comprassion2_List.Clear();
+            
+        }
+    
+        public List<double> EvenOddSort(List<double> arr)
+        {
+            bool sorted = false;
+
+            while (!sorted)
+            {
+                sorted = true;
+
+                // Четные элементы
+                for (int i = 0; i < arr.Count - 1; i += 2)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        Swap(arr, i, i + 1);
+                        sorted = false;
+                    }
+                }
+
+                // Нечетные элементы
+                for (int i = 1; i < arr.Count - 1; i += 2)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        Swap(arr, i, i + 1);
+                        sorted = false;
+                    }
+                }
+
+                comprassion2 += 2;
+            }
+
+            return arr;
+        }
+
+        public void Swap(List<double> arr, int i, int j)
+        {
+            swapping1++;
+            comprassion2++;
+            double temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+
+    private List<double> QuickSort(List<double> data, int left, int right)
+    {
+      int i, j;
+      double pivot, temp;
+      i = left;
+      j = right;
+      pivot = data[(left + right) / 2];
+
+      do
+      {
+        while ((data[i].CompareTo(pivot) < 0) && (i < right)) i++;
+        while ((pivot.CompareTo(data[j]) < 0) && (j > left)) j--;
+        if (i <= j)
+        {
+          comprassion++;
+          swapping++;
+          temp = data[i];
+          data[i] = data[j];
+          data[j] = temp;
+          i++;
+          j--;
+        }
+      } while (i <= j);
+
+      if (left < j){comprassion++; QuickSort(data, left, j);}
+      if (i < right){comprassion++; QuickSort(data, i, right);}
+      
+      return data;
+    }
+
+  }
+}
